b27cbd4969ddb23e2915f360bbfd1150491b6212
Cadmust <cadmust95@gmail.com>
2023-10-18 22:29:27 +0800

    second commit

---
+++ app/api/upload/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server'
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({ 
+//   cloud_name: process.env.CLOUDINARY_NAME, 
+//   api_key: process.env.CLOUDINARY_KEY, 
+//   api_secret: process.env.CLOUDINARY_SECRET,
+
+    // cloud_name: 'dfgntvcwz', 
+    // api_key: '438785416326731', 
+    // api_secret: 'RZQU6fmM6vmnTobiBpCiyX7vB-w' 
+
+    secure: false
+});
+
+export async function POST(request: Request) {
+    const { path } = await request.json()
+
+    if(!path) {
+        return NextResponse.json(
+            { message: 'Image path is required' },
+            { status : 400 }
+        )
+    }
+
+    try {
+        const options = {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: true,
+            transformation: [{ width: 1000, height: 752, crop: "scale" }],
+        }
+
+        const result = await cloudinary.uploader.upload(path, options)
+
+        return NextResponse.json(result, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to upload image on Cloudinary" }, { status: 500 })
+    }
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { NavLinks } from '@/constants'
-import { useSession } from 'next-auth/react'
 import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
+import { signOut } from 'next-auth/react'
+import ProfileMenu from './ProfileMenu'
 
-const Navbar = () => {
-    const session = {}
+const Navbar = async () => {
+    const session = await getCurrentUser()
+
     return (
         <nav className='flexBetween navbar'>
             <div className="flex-1 flexStart gap-10">
@@ -27,13 +30,13 @@ const Navbar = () => {
             </div>
 
             <div className="flexCenter gap-4">
-                {session ? (
+                {session?.user ? (
                     <>
-                    UserPhoto
+                        <ProfileMenu session={session}/>
 
-                    <Link href="/create-project">
-                        Share Work
-                    </Link>
+                        <Link href="/create-project">
+                            Share Link
+                        </Link>
                     </>
                 ) : (
                     <AuthProviders />
